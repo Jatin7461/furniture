5e8ae3b5305075c00528700bd883f1bc10d4f686
Jatin Gandhi <goldcoinjatin@gmail.com>
2025-01-06 21:44:48 +0530

    added chair rotation and textures

--- src/Components/Chair.jsx
+++ src/Components/Chair.jsx
@@ -1,15 +1,50 @@
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useLoader, useFrame } from '@react-three/fiber'
+import { MeshStandardMaterial, TextureLoader, Clock } from 'three'
+import { useEffect } from 'react'
+import { GUI } from 'dat.gui'
 
 const Chair = (props) => {
     const { nodes, materials } = useGLTF('blackchair.glb')
+    console.log(materials)
+    const texture = useLoader(TextureLoader, 'gltf_embedded_4.png')
+    const chairRef = useRef()
+    useEffect(() => {
+        const gui = new GUI()
+        gui.add(chairRef.current.rotation, 'x', 0, Math.PI * 2, 0.01)
+        gui.add(chairRef.current.rotation, 'y', 0, Math.PI * 2, 0.01)
+        gui.add(chairRef.current.rotation, 'z', 0, Math.PI * 2, 0.01)
+        return () => {
+            gui.destroy()
+        }
+    }, [])
+
+    const clock = new Clock()
+
+    useFrame((_, delta) => {
+        chairRef.current.rotation.x += 0.001
+        chairRef.current.rotation.y += 0.001
+        chairRef.current.rotation.z += 0.001
+    })
+
+    window.addEventListener('scroll', () => {
+        chairRef.current.rotation.x += 0.02
+        chairRef.current.rotation.y += 0.02
+        chairRef.current.rotation.z += 0.02
+    })
+
     return (
         <group {...props} dispose={null}>
             <mesh
+                ref={chairRef}
                 castShadow
                 receiveShadow
                 geometry={nodes.model.geometry}
-                material={materials.place_holder}
+                // material={materials.place_holder}
+                scale={1.5}
+                rotation={[0.29, 0.29, 0]}
+                material={new MeshStandardMaterial({ map: texture })}
             />
         </group>
     )
--- src/Components/Scene.tsx
+++ src/Components/Scene.tsx
@@ -1,10 +1,16 @@
+import { OrbitControls } from "@react-three/drei"
 import { Suspense } from "react"
 import Chair from "./Chair"
 
 const Scene = () => {
     return <>
         <Suspense>
-            <directionalLight intensity={10} />
+            <directionalLight intensity={1.5} color="#aaaaaa"
+                position={[0.5, 7.5, 2.5]}
+            />
+            {/* <OrbitControls /> */}
+            <directionalLight intensity={1.5} position={[-15, 0, -5]} />
+            {/* <ambientLight intensity={0.75} color="0xffffff" /> */}
             <Chair />
         </Suspense>
     </>
