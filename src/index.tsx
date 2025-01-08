438fd09718eb2ee632a19b876ca06e6eae2750a1
Jatin Gandhi <goldcoinjatin@gmail.com>
2025-01-08 22:29:54 +0530

    loading screen

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Suspense } from 'react'
+import LoadingScreen from './Components/LoadingScreen';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
